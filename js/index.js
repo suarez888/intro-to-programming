70d946e6dc4d1b17e13ab4d9ba1289bcf6280660
Emmely <emmelysuarez7@gmail.com>
2022-08-10 12:42:36 -0700

    added footer and skill section

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
+
 let today = new Date();
 let thisYear = today.getFullYear();
-let footer = document.querySelector('footer');
-let copyright = document.createElement('p');
-copyright.innerHTML = "thisYear";
+let footer = document.querySelector('.footer');
+let copyright = document.createElement('p')
+copyright.innerHTML = "Emmely " + thisYear;
 footer.appendChild(copyright);
 
-let skills = ["HTML","CSS","JavaScript"];
-let skillsSection = select.getElementById('skills');
-let skillsList = skillsSection.querySelector('ul');
 
-for (let i = 0; skills.length; i++ ){
-    let skill = document.createElement('li');
-    skill.innerText = [];
-    skill.appendChild(skillsList);
+var skills = ["HTML","CSS","JavaScript"];
+var skillsSection = document.getElementById('mySkills');
+var skillsList = skillsSection.querySelector('ul');
+
+for (let i = 0; i < skills.length; i++ ){
+    var skill = document.createElement('li');
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
 };
