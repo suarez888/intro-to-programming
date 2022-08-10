97092b9edf331c052e07368ce111a3961a2e8a3d
Emmely <emmelysuarez7@gmail.com>
2022-08-09 18:27:40 -0700

    added for loop and modified footer

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 let today = new Date();
 let thisYear = today.getFullYear();
-let footer = document.querySelector('.footer');
+let footer = document.querySelector('footer');
 let copyright = document.createElement('p');
-copyright.innerHTML = "Emmely.thisYear";
+copyright.innerHTML = "thisYear";
 footer.appendChild(copyright);
 
+let skills = ["HTML","CSS","JavaScript"];
+let skillsSection = select.getElementById('skills');
+let skillsList = skillsSection.querySelector('ul');
 
-
-
+for (let i = 0; skills.length; i++ ){
+    let skill = document.createElement('li');
+    skill.innerText = [];
+    skill.appendChild(skillsList);
+};
